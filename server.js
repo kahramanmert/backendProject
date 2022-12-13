e0c9be22081103c3fa34cb67ded25045488250aa
kahramanmert <kahramanmert97@hotmail.com>
2022-12-14 01:08:39 +0300

    make an ajax call

--- server.js
+++ server.js
@@ -1,15 +1,34 @@
 const express = require('express')
 const app = express()
+/*
 const data = require('./articles.json')
-const fs = require('fs');
+const fs = require('fs')
+*/
+const path = require('path')
+const cors = require('cors')
+
+app.use(cors())
 
 app.get('/', function (request,response) {
     response.send('Hello World')
 })
 
 app.get('/articles', function (request, response) {
-    response.header("Content-Type", "application/json")
-    response.send(JSON.stringify(data))
+    response.header('Content-Type', 'application/json') /*text/plain // x-www-form-urlencoded*/
+    response.set('Access-Control-Allow-Origin', '*')
+
+    /*
+    fs.readFile('./customer.json', 'utf8', (err, jsonString) => {
+        if (err) {
+            console.log("File read failed:", err)
+            return
+        }
+        console.log('File data:', jsonString)
+    })
+    */
+
+    response.sendFile(path.resolve('./articles.json'))
+    //response.send(JSON.stringify(data))
 })
 
 console.log("Server is running...")
