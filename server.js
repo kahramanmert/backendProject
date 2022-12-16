2ece4629a99530b128f99649fbd568cca263e0b1
kahramanmert <kahramanmert97@hotmail.com>
2022-12-16 19:57:52 +0300

    add imgs

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require('express')
 const app = express()
-/*
-const data = require('./articles.json')
-const fs = require('fs')
-*/
 const path = require('path')
 const cors = require('cors')
 
 app.use(cors())
+
+app.use('/static', express.static(path.join(__dirname, 'public')))
 
 app.get('/', function (request,response) {
     response.send('Hello World')
@@ -16,19 +14,7 @@ app.get('/', function (request,response) {
 app.get('/articles', function (request, response) {
     response.header('Content-Type', 'application/json') /*text/plain // x-www-form-urlencoded*/
     response.set('Access-Control-Allow-Origin', '*')
-
-    /*
-    fs.readFile('./customer.json', 'utf8', (err, jsonString) => {
-        if (err) {
-            console.log("File read failed:", err)
-            return
-        }
-        console.log('File data:', jsonString)
-    })
-    */
-
     response.sendFile(path.resolve('./articles.json'))
-    //response.send(JSON.stringify(data))
 })
 
 console.log("Server is running...")
